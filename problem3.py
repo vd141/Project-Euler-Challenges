d035569157d8cee3864e68af0c8c7de7b34c4dab
Victor Dinh <victordinh@Victors-MacBook-Air.local>
2024-06-26 19:17:43 -0700

    problem 3 solved :)

--- problem3.py
+++ problem3.py
@@ -3,33 +3,37 @@
 
 import math
 
-#target = 600851475143
-target = 13195
+target = 600851475143
+#target = 13195
 #target = 10
         
-def find_factors(target, primes_only = False):
+def find_all_factors(target):
     factors = []
-    if not primes_only:
-        for x in range(1, target+1):
-            if target%x == 0:
-                factors.append(x)
-                print(factors)
-                if target/x in factors:
-                    break
-        print('')
-        print(factors)
-        print('')
-        for x in reversed(factors[1:-2]):
-            factors.append(int(target/x))
-        return factors
-        
-    else:
-        for x in range(1, target+1):
-            if target%x == 0:
+    for x in range(1, target+1):
+        if target%x == 0:
+            factors.append(x)
+            print(factors)
+            if target/x in factors:
+                break
+    print('')
+    print(factors)
+    print('')
+    for x in reversed(factors[0:-2]):
+        factors.append(int(target/x))
+    return factors
                 
+def remove_composite_numbers(factors):
+    primes_only = []
+    for x in factors[1:-1]:
+        for y in range(2,x):
+            if x%y == 0:
+                break
+        if y == x-1:
+            primes_only.append(x)
+    return primes_only
+        
     
-def eliminate_factor_if_not_prime(factors):
-    for x in factors:
-        find_factors(x,True)
-    
-print(*find_factors(target))
+all_factors = find_all_factors(target)
+print(*all_factors)
+
+print(*remove_composite_numbers(all_factors))
