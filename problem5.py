0132eb19176b86fcc7c0a2ad099c9b82cc3f7e43
vd141 <victor.dinh141@gmail.com>
2024-06-28 15:32:23 -0700

    solved problem 5!

--- problem5.py
+++ problem5.py
@@ -2,8 +2,8 @@
 # from 1 to 10 without any remainder. What is the smallest positive number
 # that is evenly divisible by all of the numbers from 1 to 20?
 
-all_factors = range(1,21)
-i = 1
+all_factors = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+i = 2520
 
 while True:
     mod_result = [i%x for x in all_factors]
@@ -11,4 +11,6 @@ while True:
         print(i)
         break
     print(i)
-    i += 1
+    i += 2520
+
+    # got 232792560 by incrementing by 2520
