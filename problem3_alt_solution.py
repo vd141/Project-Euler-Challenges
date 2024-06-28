93821f304a4cca2fc2fd317a53b3120e25a4a2dd
Victor Dinh <victordinh@Victors-MacBook-Air.local>
2024-06-28 13:11:39 -0700

    adding problem 3 alternate solution (more elegant)

---
+++ problem3_alt_solution.py
@@ -0,0 +1,11 @@
+n = 1155
+factor = 2
+lastFactor = 1
+while n>1:
+    if n%factor == 0:
+        lastFactor = factor
+        n = n/factor
+        while n%factor == 0:
+            n = n/factor
+    factor = factor+1
+print(lastFactor)
